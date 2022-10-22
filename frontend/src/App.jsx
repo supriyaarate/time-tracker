034bfbfa3b5bc3728f09eb8a334a4cac0757f6c7
Subh Paul <subh1909@gmail.com>
2022-10-22 14:50:52 -0400

    Resolving conflicts and modifying gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import "./App.css";
 import LandingPage from "./views/LandingPage/LandingPage";
 
+
 const App = ()  => {
 
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <LandingPage  />
+        </div>
+    );
 }
 
 export default App;
