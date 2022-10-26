ccb9caf70b7fc55ee268e39a4ab8a9c884ee2214
radhika1596 <radhi1596@gmail.com>
2022-10-25 23:39:56 -0400

    i18n setup complete with few translations

--- frontend/src/views/LandingPage/LandingPage.jsx
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import Grid from "@mui/material/Grid";
 import Login from "../Login/Login";
+import "./../../translations/i18n";
+import { useTranslation } from "react-i18next";
 
 const LandingPage = () => {
+  const { t } = useTranslation();
+
   return (
     <Grid container spacing={2} columns={16}>
       <Grid item xs={8}>
-        <p>Hi</p>
+        <p>{t("hi")}</p>
       </Grid>
       <Grid item xs={8}>
         <Login />
