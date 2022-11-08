60fbd77b04cc8c1818a8d82c8146129c1f3ff19d
Thu Tran <erictr0922@gmail.com>
2022-11-07 20:32:41 -0500

    add home page

--- frontend/src/components/Body/Body.js
+++ frontend/src/components/Body/Body.js
@@ -1,110 +1,5 @@
-/* eslint-disable prettier/prettier */
-// import React from "react";
-// import "./Body.css";
-// import clock from "../../assest/clock.gif";
-// import lists from "../../assest/lists.gif";
-// import pencil from "../../assest/pencil.gif";
-// import profile from "../../assest/profile.gif";
-// import { Box, Typography, Paper,Grid } from "@mui/material";
-// import { createTheme } from "@mui/material/styles";
-
-// const theme = createTheme({
-//   typography: {
-//     h5: {
-//       color: "#00687B",
-//     },
-//   },
-// });
-
-// const Body = () => {
-//   console.log(theme);
-//   return (
-//     <>
-//       <Grid container spacing={2}>
-//   <Grid item xs={8}>
-
-//       <Box
-//         sx={{
-//           backgroundColor: "#E5E5E5",
-//           height: "100vh",
-//         }}
-//       >
-//         <Paper
-//           elevation={0}
-//           sx={{
-//             margin: "0 70px",
-//           }}
-//         >
-//           <Box
-//             sx={{
-//               display: "flex",
-//               flexDirection: "row",
-//               justifyContent: "center",
-//               alignItems: "center",
-//             }}
-//           >
-//             <Box
-//               sx={{
-//                 display: "flex",
-//                 flexDirection: "column",
-//                 width: "10rem",
-//               }}
-//             >
-//               <img src={clock} alt="clock" />
-//               <Typography variant="h5" element="p">
-//                 Current job
-//               </Typography>
-//             </Box>
-//             <Box
-//               sx={{
-//                 display: "flex",
-//                 flexDirection: "column",
-//                 width: "10rem",
-//               }}
-//             >
-//               <img src={pencil} alt="pencil" />
-//               <Typography variant="h5" element="p">
-//                 Add/edit jobs
-//               </Typography>
-//             </Box>
-//             <Box
-//               sx={{
-//                 display: "flex",
-//                 flexDirection: "column",
-//                 width: "10rem",
-//               }}
-//             >
-//               <img src={lists} alt="lists" />
-//               <Typography variant="h5" element="p">
-//                 Payment/Invoice (comming soon)
-//               </Typography>
-//             </Box>
-//             <Box
-//               sx={{
-//                 display: "flex",
-//                 flexDirection: "column",
-//                 width: "10rem",
-//               }}
-//             >
-//               <img src={profile} alt="profile" />
-//               <Typography variant="h5" element="p">
-//                 Profile
-//               </Typography>
-//             </Box>
-//           </Box>
-//         </Paper>
-//       </Box>
-//   </Grid>
-
-// </Grid>
-//     </>
-//   );
-// };
-
-// export default Body;
-
-import React from "react";
-import { Grid, Typography, Stack, Paper, Box } from "@mui/material";
+import * as React from "react";
+import { Stack, Typography, Paper, Grid, Box } from "@mui/material";
 import clock from "../../assest/clock.gif";
 import lists from "../../assest/lists.gif";
 import pencil from "../../assest/pencil.gif";
@@ -113,10 +8,19 @@ import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
   img: {
-    width: "210px",
+    width: "100%",
     height: "100%",
+    alignItems: "center",
   },
-  text: {
+  myBox: {
+    height: "90vh",
+    display: "flex",
+    alignItems: "center",
+  },
+  boxContainer: {
+    backgroundColor: "#E5E5E5",
+  },
+  textColor: {
     color: "#00687B",
   },
 });
@@ -124,95 +28,67 @@ const useStyles = makeStyles({
 const Body = () => {
   const classes = useStyles();
   return (
-    <Box
-      sx={{
-        backgroundColor: "#E5E5E5",
-      }}
-    >
-      <Grid
-        container
-        alignItems="center"
-        justifyContent="center"
-        style={{ height: "90vh" }}
-      >
-        <Grid item md={2} sm="auto" xs="auto">
-          <Paper
-            variant="outlined"
-            elevation={3}
-            sx={{
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Stack alignItems="center" py={1} pb={3}>
-              <img src={clock} alt="pencil" className={classes.img} />
-              <Typography variant="h5" element="p" className={classes.text}>
-                Current job
-              </Typography>
-            </Stack>
-          </Paper>
+    <Box className={classes.boxContainer}>
+      <Box className={classes.myBox}>
+        <Grid container justifyContent="center">
+          <Grid item xs={2}>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Stack alignItems="center" py={2}>
+                <img src={clock} className={classes.img}></img>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  className={classes.textColor}
+                >
+                  Current job
+                </Typography>
+              </Stack>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Stack alignItems="center" py={2}>
+                <img src={pencil} className={classes.img}></img>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  className={classes.textColor}
+                >
+                  Add/edit jobs
+                </Typography>
+              </Stack>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Stack alignItems="center" py={2}>
+                <img src={lists} className={classes.img}></img>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  className={classes.textColor}
+                >
+                  Payment/Invoice (comming soon)
+                </Typography>
+              </Stack>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <Stack alignItems="center" py={2}>
+                <img src={profile} className={classes.img}></img>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  className={classes.textColor}
+                >
+                  Profile
+                </Typography>
+              </Stack>
+            </Paper>
+          </Grid>
         </Grid>
-
-        <Grid item md={2} sm="auto" xs="auto">
-          <Paper
-            variant="outlined"
-            elevation={3}
-            sx={{
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Stack alignItems="center" py={1} pb={3}>
-              <img src={pencil} alt="pencil" className={classes.img} />
-              <Typography variant="h5" element="p" className={classes.text}>
-                Add/edit jobs
-              </Typography>
-            </Stack>
-          </Paper>
-        </Grid>
-        <Grid item md={2} sm="auto" xs="auto">
-          <Paper
-            variant="outlined"
-            elevation={3}
-            sx={{
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Stack alignItems="center" py={1} pb={3}>
-              <img src={lists} alt="pencil" className={classes.img} />
-              <Typography variant="h5" element="p" className={classes.text}>
-                Payment/Invoice
-              </Typography>
-            </Stack>
-          </Paper>
-        </Grid>
-        <Grid item md={2} sm="auto" xs="auto">
-          <Paper
-            variant="outlined"
-            elevation={3}
-            sx={{
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Stack alignItems="center" py={1} pb={3}>
-              <img src={profile} alt="pencil" className={classes.img} />
-              <Typography variant="h5" element="p" className={classes.text}>
-                Profile
-              </Typography>
-            </Stack>
-          </Paper>
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
